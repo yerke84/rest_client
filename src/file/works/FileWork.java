aa6d5bb2161aa1bdede88c9e0dc6bba6485a6e01
yerke84 <yerke1984@gmail.com>
2018-11-07 16:26:32 +0600

    3

--- src/file/works/FileWork.java
+++ src/file/works/FileWork.java
@@ -2,6 +2,10 @@ package file.works;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
@@ -23,20 +27,23 @@ public class FileWork {
 		super();
 	}
 
-	public static byte[] getFileAsByteArray(String url) {
+	public static boolean downloadFromUrlToFile(String url, String location) {
 
-		byte[] binaryData = null;
+		boolean ret = false;
 		try {
-			URL u = new URL(url);
-			int contentLength = u.openConnection().getContentLength();
-			InputStream openStream = u.openStream();
-			binaryData = new byte[contentLength];
-			openStream.read(binaryData);
-			openStream.close();
+			URL website = new URL(url);
+			InputStream in = website.openStream();
+			Path path = Paths.get(location);
+			Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
+			try {
+				in.close();
+			} catch (Exception ei) {
+			}
+			ret = true;
 		} catch (Exception e) {
 			LOGGER.log(Level.SEVERE, e.getMessage());
 		}
-		return binaryData;
+		return ret;
 	}
 
 	private static void disableSslVerification() {
@@ -63,6 +70,7 @@ public class FileWork {
 
 			// Create all-trusting host name verifier
 			HostnameVerifier allHostsValid = new HostnameVerifier() {
+
 				public boolean verify(String hostname, SSLSession session) {
 					return true;
 				}
@@ -70,7 +78,9 @@ public class FileWork {
 
 			// Install the all-trusting host verifier
 			HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-		} catch (NoSuchAlgorithmException e) {
+		} catch (
+
+		NoSuchAlgorithmException e) {
 			LOGGER.log(Level.SEVERE, e.getMessage());
 		} catch (KeyManagementException e) {
 			LOGGER.log(Level.SEVERE, e.getMessage());
--- src/file/works/testFile.java
+++ src/file/works/testFile.java
@@ -9,9 +9,7 @@ public class testFile {
 	// https://github.com/yerke84/rest_client/blob/master/testFile/%D0%9D%D1%83%D1%80%D0%BB%D1%8B%20%D0%B6%D0%B5%D1%80.xlsx?raw=true
 	public void test() {
 		String url = "https://github.com/yerke84/rest_client/blob/master/testFile/%D0%9D%D1%83%D1%80%D0%BB%D1%8B%20%D0%B6%D0%B5%D1%80.xlsx?raw=true";
-		byte[] arr = FileWork.getFileAsByteArray(url);
-		String str = arr.toString();
-		assertEquals("", str);
+		assertEquals("", FileWork.downloadFromUrlToFile(url, "/tmp/01.xlsx"));
 
 	}
 
